3a2c8b1ccfd1b136e5a96ee7187a966dcb2189e2
David-Kirsch <davidjkirsch@gmail.com>
2020-08-02 15:39:26 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do
+        "My name is David"
+      end
+    
+      get '/hometown' do
+        "My hometown is Detroit"
+      end
+    
+      get '/favorite-song' do
+        "My favorite song is I Choose You - Sara Bareilles"
+      end
 end
