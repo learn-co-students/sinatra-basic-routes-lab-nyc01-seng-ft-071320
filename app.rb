4408cdf3c7b37ff5a42476db3cfd1dff9b0c41ad
vincentyang01 <vincentyangcsi@gmail.com>
2020-08-01 22:56:49 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Vincent!"
+    end
+    get '/hometown' do
+        "My hometown is Montrose!"
+    end
+    get '/favorite-song' do
+        "My favorite song is something!"
+    end 
 end
